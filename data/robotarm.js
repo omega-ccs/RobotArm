b163abe8a5bbb336cb5e8185ac6f54b78a5bbdd8
Hugo Larson <hugo.larson@columbiachristian.com>
2026-03-05 08:16:38 -0800

    Built out websockets in client and device

--- data/robotarm.js
+++ data/robotarm.js
@@ -1,9 +1,12 @@
 var servo_pos_rotation = 90;
 var servo_pos_shoulder = 90;
+var servo_pos_claw = 90;
 
 // Canvas element and its 2d context
 var canvas;
 var context;
+
+var ws;
 
 function robotarm() {
     canvas = document.getElementById("canvas")
@@ -18,15 +21,18 @@ function robotarm() {
     window.addEventListener("gamepadconnected", (event) => {
         console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
                     event.gamepad.index, event.gamepad.id,
-                    event.gamepad.buttons.length, event.gamepad.axes.length);
+            event.gamepad.buttons.length, event.gamepad.axes.length);
+        console.log(event.gamepad.id)
     }, false);
+
+    ws = new WebSocket(`ws://${robotarm_hostname}/ws`)
 
     // Run the main loop
     draw();
 }
 
-var arm = "robotarm.local"
-//var arm = "10.0.128.97"
+var robotarm_hostname = "robotarm.local"
+//var robotarm_hostname = "10.0.128.97"
 
 function draw() {
     // Clear the canvas
@@ -38,6 +44,16 @@ function draw() {
         // Grab the X and Y axes and adjust them to the servo range
         servo_pos_rotation = (gamepads[0].axes[0] * -90) + 90;
         servo_pos_shoulder = (gamepads[0].axes[1] * -90) + 90;
+
+        var clawbutton;
+        //console.log(gamepads[0].buttons.map(button => button.pressed))
+        if (gamepads[0].id == "Saitek ST90 USB Stick (Vendor: 06a3 Product: 0422)")
+            clawbutton = gamepads[0].buttons[0]
+        else
+            clawbutton = gamepads[0].buttons[42]
+
+        // Set claw servo to 67 if pressed, otherwise to 42
+        servo_pos_claw = clawbutton.pressed ? 67 : 42
     }
 
     // Draw the servo positions to the screen
@@ -46,11 +62,21 @@ function draw() {
     context.fillText(servo_pos_rotation,100,100);
     context.fillText(servo_pos_shoulder,100,200);
 
-    //
-    fetch(`http://${arm}/servo?rotation=${servo_pos_rotation}&shoulder=${servo_pos_shoulder}`)
-//        .then(response => {
-//            console.log("completed fetch");
-//        })
+    var ws_message = {
+        "rotation": servo_pos_rotation,
+        "shoulder": servo_pos_shoulder,
+        "claw": servo_pos_claw
+    }
+    
+    // only if the websocket is open and ready, send messages
+    if (ws.readyState == WebSocket.OPEN) {
+        ws_message_json = JSON.stringify(ws_message)
+        console.log(`sending ws message ${ws_message_json}`)
+        ws.send(ws_message_json)
+    }
+    
+    // use fetch/HTTP to send servo positions
+    fetch(`http://${robotarm_hostname}/servo?rotation=${servo_pos_rotation}&shoulder=${servo_pos_shoulder}`)
 
     // Either request the browser call this function ASAP
 //    window.requestAnimationFrame(draw);
